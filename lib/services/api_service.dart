711e12d6b8f60fbe521d6b4d6fd45a144101f1b7
baor69420 <naimiu123@gmail.com>
2026-02-20 17:14:45 +0700

    added detection when app is background

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = "http://192.168.2.221:8000";
+  static const String baseUrl = "http://192.168.1.52:8000";
   static const String _tokenKey = "api_token";
 
   static String token = "";
